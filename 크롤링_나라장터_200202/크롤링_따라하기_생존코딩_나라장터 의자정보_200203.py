9e638742f93c4f0fd3bdb66f6ca5fbd744d32ee8
951237 <951237@gmail.com>
2020-02-03 14:08:29 +0900

    Doc : Update - 윈두우용 크롬드라이버 경로 추가

--- 크롤링_나라장터_200202/크롤링_따라하기_생존코딩_나라장터 의자정보_200203.py
+++ 크롤링_나라장터_200202/크롤링_따라하기_생존코딩_나라장터 의자정보_200203.py
@@ -11,8 +11,9 @@ from selenium.common.exceptions import *
 
 DOWNLOAD_DIR = '/Users/mac/Documents/python_work/my_project/crawling/크롤링_' \
                '나라장터_200202/imgs '
-driver = webdriver.Chrome('/Users/mac/Documents/python_work/my_project'
-                          '/crawling/chromedriver_mac')
+# driver = webdriver.Chrome('/Users/mac/Documents/python_work/my_project'
+#                          '/crawling/chromedriver_mac')
+driver = webdriver.Chrome(r'C:\Users\User\PycharmProjects\python_crawling\chromedriver_win.exe')
 driver.get('http://shopping.g2b.go.kr')  # 나라장터 사이트로 이동
 driver.switch_to.frame('sub')  # 메인프레임속의 서브프레임으로 바꾸기
 
@@ -29,7 +30,7 @@ driver.execute_script("javascript:attrNmValLink('5611210201', '등판재질',  "
     'ul#dLstDiv>li>input[type="checkbox"]')  # 체크박스의
 # 텍스트가 인풋 태그 밖에 위치하여 체크박스를 모두 찾음.
 
-for 체크박스 in 체크박스리스트:
+for 체크박스 in 체크박스리스트: # 왜 안될까?
     parent = 체크박스.find_element_by_xpath('./..')  # 체크박스 상위를 부모로 변수를 만듦
     for i in 필수옵션:  # 필수옵션 리스트 2개 확인
         if i in parent.text and '가죽' not in parent:  # '메시'나 '망사'를 포함하지만,
