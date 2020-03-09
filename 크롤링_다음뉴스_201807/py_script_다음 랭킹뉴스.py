c36463e0d759f144576c5c37a0e6e8c1c53e8981
951237 <951237.gmail.com>
2020-03-09 22:57:00 +0900

    Docs - Create

--- 크롤링_다음뉴스_201807/py_script_다음 랭킹뉴스.py
+++ 크롤링_다음뉴스_201807/py_script_다음 랭킹뉴스.py
@@ -2,21 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from urllib import request
+
 from bs4 import BeautifulSoup as soup
+
 
 def get_html(url):
     html = request.urlopen(url)
     bs_obj = soup(html, "html.parser")
     return bs_obj
 
+
 url = 'https://media.daum.net/ranking/popular/'
 bs_obj = get_html(url)
 
+
 def dnew_rank_pop():
-    all_news = bs_obj.find('div',{'class':'rank_news'})
+    all_news = bs_obj.find('div', {'class': 'rank_news'})
     # print(len(all_news))
     title = all_news.find('h4')
-    all_a = all_news.findAll('a',{'class':'link_txt'})
+    all_a = all_news.findAll('a', {'class': 'link_txt'})
 
     a = print(title.text)
 
@@ -26,8 +30,6 @@ def dnew_rank_pop():
         print()
         i += 1
 
+
 dnew_rank_pop()
 input()
-
-
-
