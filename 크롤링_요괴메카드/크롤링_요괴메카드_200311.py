309b6844eade7f5f88fba81040ec170cf6481f49
951237 <951237.gmail.com>
2020-03-12 02:39:17 +0900

    Doc - Update - 캐릭터 이미지 다운로드 완료 - 추가작업 	- 카테고리 별로 폴더생성하여 저장하기

--- 크롤링_요괴메카드/크롤링_요괴메카드_200311.py
+++ 크롤링_요괴메카드/크롤링_요괴메카드_200311.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 from urllib import request
 
+import requests
 from bs4 import BeautifulSoup as soup
 
-from datetime import datetime
-
-url = 'http://ghostmecard.choirock.com/character.php'
+URL = 'http://ghostmecard.choirock.com/character.php'
+BASE_URL = 'http://ghostmecard.choirock.com'
+테이머 = []
+tabs = {
+    'tab1': '테이머',
+    'tab2': '십이지정령',
+    'tab3': '수호정',
+    'tab4': '요괴',
+    'tab5': '왕마요괴'
+}
 
 
 def get_html(url):
@@ -17,10 +25,47 @@ def get_html(url):
 
 bs_obj = get_html(url)
 
-# 캐릭터 팝업창 정보 수
+# 캐릭터 세부정보 수집
 popups = bs_obj.findAll('div', {'class': 'popup_info'})
 
 
+# 이미지 링크 수집하
+def get_link(imgs, _list):
+    for img in imgs:
+        dic = {
+            'name': img.get('alt'),
+            'link': img.get('src')
+        }
+        _list.append(dic)
+
+
+# 테이머 이름과 이미지 링크 수집
+for k, v in tabs.items():
+    v = []
+    k = bs_obj.find('div', {'id': k})
+    imgs = k.findAll('img')
+    get_link(imgs, v)
+    테이머.append(v)
+
+
+# 이미지 파일 저장하기
+def download_img(_list):
+    for i in _list:
+        name = i['name']
+        link = BASE_URL + i['link']
+        with open(f'{name}.png', 'wb') as file:
+            response = requests.get(link, stream=True)
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                file.write(block)
+
+
+# 캐릭터 세부정보 수집 popups
 def make_dic(popups):
     # 12지신 정보만 수집 < 배경 >의 특징이 있음.
     _list = []
@@ -35,6 +80,7 @@ def make_dic(popups):
     return _list
 
 
+# 캐릭터 정보 화면 출력
 def show_contents(_list):
     for lst in _list:
         print(lst['name'])  # 12지 정령 이름 출력
@@ -43,6 +89,7 @@ def show_contents(_list):
         print()
 
 
+# 캐릭터 정보 텍스트파일 저장하기
 def make_text(_list):
     file_name = f'태원_요괴메카드_12지신_{datetime.now()}.txt'
     print(f'{file_name} 파일 작성 시작!')
@@ -51,14 +98,15 @@ def make_text(_list):
         nl = '\n'
         tb = '\t'
         _r = '\r'
-        txt_file.write(f'{lst["name"]}{nl}') #12정령 이름 작성
-        for a in lst["disc"]:   # 12지 정령 내용 기록 시작
+        txt_file.write(f'{lst["name"]}{nl}')  # 12정령 이름 작성
+        for a in lst["disc"]:  # 12지 정령 내용 기록 시작
             txt_file.write(f'{tb}{a.replace(_r, "")}{nl}')
         txt_file.write(f'{nl}')
     txt_file.close()
     print(f'{file_name} 파일 작성 완료!')
 
-
-_list = make_dic(popups)    # 딕셔너리 생성
-show_contents(_list)    # 화면 출력하기
+# _list = make_dic(popups)    # 딕셔너리 생성
+# show_contents(_list)    # 화면 출력하기
 # make_text(_list)      # 파일 저장하기
+
+#
