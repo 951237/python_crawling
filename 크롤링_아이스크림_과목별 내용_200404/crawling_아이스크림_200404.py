8ee889925ff715106f28c8e13c7ec369dec10f0e
951237 <951237@gmail.com>
2020-04-06 01:39:32 +0900

    Doc : Update - 각학년 학기별 주제 모으기 완성 - todo 	- 파일에 학년, 학기, 과목이 기록되지 않음.

--- 크롤링_아이스크림_과목별 내용_200404/crawling_아이스크림_200404.py
+++ 크롤링_아이스크림_과목별 내용_200404/crawling_아이스크림_200404.py
@@ -14,65 +14,117 @@ def excute_script(script):
             sleep(1)
 
 
-def get_html(url):
-    html = request.urlopen(url)
-    bs_obj = soup(html, "html.parser")
-    return bs_obj
+dic_grade = {
+    "1학년": "wiseLogCommon.clickDataSend('20');fnSearchList(1, '', '', '');",
+    "2학년": "wiseLogCommon.clickDataSend('21');fnSearchList(2, '', '', '');",
+    "3학년": "wiseLogCommon.clickDataSend('22');fnSearchList(3, '', '', '');",
+    "4학년": "wiseLogCommon.clickDataSend('23');fnSearchList(4, '', '', '');",
+    "5학년": "wiseLogCommon.clickDataSend('24');fnSearchList(5, '', '', '');",
+    "6학년": "wiseLogCommon.clickDataSend('25');fnSearchList(6, '', '', '');"
+}
+
+dic_semestar = {
+    "1학기": "clickLogConvert(2,0);fnSearchList('', 1, '', '');",
+    "2학기": "clickLogConvert(2,1);fnSearchList('', 2, '', '');"
+}
 
 URL = 'http://www.i-scream.co.kr/user/main/MainPage.do'
 
-driver = webdriver.Chrome('/Users/mac/Documents/python_work/my_project/crawling/chromedriver_mac')    # 맥용 크롬드라이버 추가
+driver = webdriver.Chrome(
+    '/Users/mac/Documents/python_work/my_project/crawling/chromedriver_mac')  # 맥용 크롬드라이버 추가
 driver.get(URL)
 sleep(1)
 
 # 로그인
-driver.find_element_by_css_selector("input#id").send_keys(input('아이디 입력 : ')) # 아이디입력
-driver.find_element_by_css_selector("input#pw").send_keys(input('패스워드 입력 : ')) # 비밀번호 입력
-excute_script("wiseLogCommon.clickDataSend('131');login();") #로그인 버튼 클릭
+driver.find_element_by_css_selector("input#id").send_keys(
+    input('아이디 입력 : '))  # 아이디입력
+driver.find_element_by_css_selector("input#pw").send_keys(
+    input('패스워드 입력 : '))  # 비밀번호 입력
+excute_script("wiseLogCommon.clickDataSend('131');login();")  # 로그인 버튼 클릭
 sleep(1)
 
 # 페이지 이동 - 교과활동
-driver.get('http://www.i-scream.co.kr/user/subject/SubjectChasiList.do') #
+driver.get('http://www.i-scream.co.kr/user/subject/SubjectChasiList.do')  #
+
+test_onclick = "wiseLogCommon.clickDataSend('20');fnSearchList(1, '', '', '');"
+
 
 # 학년 선택
-excute_script("wiseLogCommon.clickDataSend('20');fnSearchList(1, '', '', '');") # 교과활동 - 1학년 선택
-html =driver.page_source # 페이지 소스 가져오기
+def get_soup(onclick):
+    excute_script(onclick)  # 교과활동 - 1학년 선택
+    sleep(2)
+    html = driver.page_source  # 페이지 소스 가져오기
 
-bs_obj = soup(html, 'html.parser') # 뷰티풀숩 오브젝트로 가져오기
+    bs_obj = soup(html, 'html.parser')  # 뷰티풀숩 오브젝트로 가져오기
 
-tab_area = bs_obj.find('div', {'class' : 'tab_area'}) # 단원 전체 영역 찾기
-div_all = tab_area.findAll('div', {'class' : 'toggle_list curriculum1'}) # 단원 영역 모두 찾기
+    return bs_obj
+
+
+def get_subject_list(onclick):
+    bs_obj = get_soup(onclick)
+
+    # 0.학기, 단원명 링크 가져오기
+    nav = bs_obj.find('nav', {'id': 'SkipToLNB'})
+
+    # 2. 단원명과 링크 가져오기
+    lis = nav.select("ul > li > a")
+
+    sub_links = {}
+    for li in lis:
+        a = li.text.strip()
+        onclick = li.get("onclick")
+        if onclick.split('.')[0] == 'wiseLogCommon':
+            sub_links[a] = onclick
+    return sub_links
+
+
+def display_contents(sub_links):
+    f = open('교과목 주제모음.txt','w')
+    for k, v in sub_links.items():
+        print(f'{k} 출력합니다.')
+        bs_obj = get_soup(v)
+        tab_area = bs_obj.find('div', {'class': 'tab_area'})  # 단원 전체 영역 찾기
+        # titles = tab_area.select("div > a > strong")
+        contents = tab_area.select("div > ul > li > ul > li > a")
+
+        for i in contents:
+            print(i.text.strip(), file=f)
+        print(file=f)
+    f.close()
+
+
+for k, v in dic_grade.items():
+    print(f'{k} 정보를 수집합니다.','\n')
+    excute_script(v)
+    for kk, vv in dic_semestar.items():
+        print(f'{kk} 내용을 수집합니다.')
+        sub_links = get_subject_list(vv)
+        display_contents(sub_links)
+
+
+
+print(dic_semestar.keys())
+
 
 # 단원 영역에서 차시 추출하기
 for div in div_all:
 
-    title = div.find('a').text.strip().split() # 단원명 추출하기, 여백을 없애고, 빈칸으로 나누
-    print(' '.join(title)) # 빈칸으로 나뉘어진 텍스틀 리스트를 뛰어쓰기로 출력하기
+    lis = div.select("div > ul > li > ul > li")
+    for li in lis:
+        text = li.text.strip()
+        print(text)
+
+    title = div.find('a').text.strip().split()  # 단원명 추출하기, 여백을 없애고, 빈칸으로 나누
+    print(' '.join(title))  # 빈칸으로 나뉘어진 텍스틀 리스트를 뛰어쓰기로 출력하기
 
     li_all = div.select("li")
     for i in li_all:
         print(i.find('a').text.strip())
     print()
 
-
-'''
-1학년 1학기 과목 
-wiseLogCommon.clickDataSend('2000');fnSearchList('', '', '237627', '','국어', 'ajax');
-wiseLogCommon.clickDataSend('2003');fnSearchList('', '', '237631', '','수학', 'ajax');
-wiseLogCommon.clickDataSend('20014');fnSearchList('', '', '237635', '','통합교과', 'ajax');
-wiseLogCommon.clickDataSend('20020');fnSearchList('', '', '237589', '','안전한 생활', 'ajax');
-wiseLogCommon.clickDataSend('20021');fnSearchList('', '', '30', '','입학 초기 적응활동', 'ajax');
-wiseLogCommon.clickDataSend('20011');fnSearchList('', '', '126313', '','창의', 'ajax');
-wiseLogCommon.clickDataSend('2004');fnSearchList('', '', '172627', '','교과학습 보충자료', 'ajax');
-
-1학년 2학기 과목
-wiseLogCommon.clickDataSend('2010');fnSearchList('', '', '237668', '','국어', 'ajax');
-wiseLogCommon.clickDataSend('2013');fnSearchList('', '', '237669', '','수학', 'ajax');
-wiseLogCommon.clickDataSend('20114');fnSearchList('', '', '237670', '','통합교과', 'ajax');
-wiseLogCommon.clickDataSend('20120');fnSearchList('', '', '237589', '','안전한 생활', 'ajax');
-wiseLogCommon.clickDataSend('20111');fnSearchList('', '', '126313', '','창의', 'ajax');
-
-
-'''
-excute_script("wiseLogCommon.clickDataSend('21');fnSearchList(2, '', '', '');") # 교과활동 - 2학년 선택
-excute_script("wiseLogCommon.clickDataSend('22');fnSearchList(3, '', '', '');") # 교과활동 - 3학년 선
+# # 1. 학기 선택하기
+# semesters = nav.select("ul > li > ol > li > a")
+# for i in semesters:
+#     s = i.text.strip()
+#     t = i.get("onclick")
+#     print(s, t)
