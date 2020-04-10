3e5534d5102aa8c435a50a79c8d8948a64020131
951237 <951237@gmail.com>
2020-04-10 14:47:33 +0900

    Doc : Update - 뉴스 출력시 텍스트, 출처, 링크가 출력되도록 수정함.

--- 크롤링_다음뉴스_201807/py_script_다음뉴스_180730.py
+++ 크롤링_다음뉴스_201807/py_script_다음뉴스_180730.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 최종 수정 20.04.10
+
 # 크롤링 - 다음뉴스 댓글 많은 뉴스
 from urllib import request
 from bs4 import BeautifulSoup
@@ -10,66 +12,79 @@ url = "https://media.daum.net/"
 html = request.urlopen(url)
 bs_obj = BeautifulSoup(html, "html.parser")
 
-# 뉴스 크롤링 - 다음 댓글많은 뉴스
+# 뉴스 크롤링 - 다음 열독률 높은 뉴스
 def dnews_cmt():
     all_pop_cmts = bs_obj.findAll('div',{'class':'pop_news pop_cmt'})[0]
     title = all_pop_cmts.find('h3')
-    all_a = all_pop_cmts.findAll('a',{'class':'link_txt'})
+    lis  = all_pop_cmts.select('ol > li')
+    print(title.text, '\n')
 
-    a = print(title.text)
+    for li in lis:
+        content = li.find_all('span')
+        rank = content[0].text
+        source = content[1].text
+        link = li.find('a').get('href')
+        txt = li.text.strip().replace("                    ","").replace("\n", "  ").replace("         "," :").replace("       "," /")
+        print(txt, link)
+        print()
 
-    for a_tag in all_a:
-        a_tag = a_tag.text.lstrip().replace("                        ","").replace("\n", "  ")
-        b = print(a_tag)
-    return (a, b)
 
+# 댓글 많은 뉴스
 def dnews_tit():
     all_pop_tit = bs_obj.findAll('div',{'class':'pop_news pop_cmt'})[1]
-    title = all_pop_tit.find('h3')
-    all_a = all_pop_tit.findAll('a',{'class':'link_txt'})
+    title = all_pop_tit.find('h3').text
+    lis = all_pop_tit.select('ol > li')
 
-    a = print(title.text)
+    print(title,'\n') # 댓글많은 뉴스 출력
 
-    for a_tag in all_a:
-        a_tag = a_tag.text.lstrip().replace("                    ","").replace("\n", "  ")
-        b = print(a_tag)
-    return (a, b)
+
+    for li in lis:
+        content = li.find_all('span')
+        rank = content[0].text
+        source = content[1].text
+        link = li.find('a').get('href')
+        txt = li.text.strip().replace("                    ", "").replace("\n", "  ").replace("         ", " :").replace(
+            "       ", " /")
+        print(txt, link)
+        print()
 
 # 뉴스 크롤링 - 다음 연령별 뉴스
 def dnews_age():
     all_pop_ages = bs_obj.find('div', {'class': 'pop_news pop_age'})
+    dic_gener = {
+        '여성' : 'list_agenews list_female',
+        '남성' : 'list_agenews list_male'
+    }
 
-    ul_femal = all_pop_ages.find('ul', {'class': 'list_agenews list_female'})
-    lis_news = ul_femal.findAll('a')
-    i = 1
+    for k, v in dic_gener.items():
+        ul_femal = all_pop_ages.find('ul', {'class': v})
+        lis_news = ul_femal.findAll('a')
+        i = 1
 
-    print("Daum 연령별 인기뉴스 : 여성")
-    for li in lis_news:
-        print(str(10 * i) + "대 :", li.text)
-        i = i + 1
+        print(f'Daum 연령별 인기뉴스 : {k}','\n')
+        for li in lis_news:
+            txt = li.text
+            link = li.get('href')
+            print(f'{str(10 * i + 10)}대 : {li.text} / {link}')
+            i = i + 1
+            print()
+        print("")
 
-    a = print("")
-
-    b = print("Daum 연령별 인기뉴스 : 남성")
-    ul_male = all_pop_ages.find('ul', {'class': 'list_agenews list_male'})
-    lis_news = ul_male.findAll('a')
-    i = 1
-    for li in lis_news:
-        c = print(str(10 * i) + "대 :", li.text)
-        i = i + 1
-    return (a, b, c)
 
 # 뉴스 크롤링 - 다음 이시각 뉴스
 def dnews_now():
     box_headline = bs_obj.find("div", {"class": "box_headline"})
-    all_a = box_headline.find_all("a", {"class": "link_txt"})
+    lis = box_headline.select('ul > li')
+
+    print("Daum 이시간 주요 뉴스", '\n')
     i = 1
-    a = print("Daum 이시간 주요 뉴스")
-    for a_tag in all_a:
-        a_tag = a_tag.text.replace('\n', '').strip()
-        b = print(str(i) + ".", a_tag)
-        i = i + 1
-    return (a,b) #출력값 리턴하기
+    for li in lis:
+        content = li.find_all('strong')
+        txt = content[0].text.strip().replace('\n'," / ")
+        link = li.find('a').get('href')
+        print(f'{i}. {txt} / {link}')
+        print()
+        i += 1
 
 
 dnews_now()
