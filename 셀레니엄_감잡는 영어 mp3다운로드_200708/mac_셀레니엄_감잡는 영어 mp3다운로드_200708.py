54da1d45b4e19e05c713ddc8d56e8e91e2cc8203
951237 <951237@gmail.com>
2020-07-14 07:38:06 +0900

    Doc : Create - 1차 완성 - 전체를 다운받지 못하고 실행도중 멈춤 / sleep시간 조절하기 - todo : 리팩토리 및 주석 달기

--- 셀레니엄_감잡는 영어 mp3다운로드_200708/mac_셀레니엄_감잡는 영어 mp3다운로드_200708.py
+++ 셀레니엄_감잡는 영어 mp3다운로드_200708/mac_셀레니엄_감잡는 영어 mp3다운로드_200708.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 import time
 import pyautogui as pa
 
+pa.FAILSAFE = True
 
 URL = 'http://www.teacher21.co.kr/'
 URL_LECTURE = 'http://www.teacher21.co.kr/classroom/lecture_hojoon.asp'
@@ -15,6 +16,79 @@ driver = webdriver.Chrome('/Users/mac/Documents/coding_python/python_crawling/ch
 print('사이트에 접속중..')
 driver.get(URL)
 
+driver.get(URL_LECTURE)
+
+driver.find_element_by_xpath()
+
+# xpath 출력 해보기
+for i in range(29, 61):
+    v_xpath = f'//*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[{i*2 + 6}]/td[3]'
+    driver.find_element_by_xpath(v_xpath).click()
+    time.sleep(3)
+
+    # 주행하기 클
+    # 주행하기 = pa.screenshot(region=(736, 177, 30, 30) ) # 강의실 입장하기 좌표
+    img_주행하기 = pa.locateCenterOnScreen('drive.png')  # 이미지파일 센터값 저장
+    time.sleep(0.25)
+
+    pa.click(img_주행하기) # 강의실 입장 센터값 클릭
+    pa.click(img_주행하기) # 강의실 입장 센터값 클릭
+    time.sleep(1)
+
+    # 렛쯔 스터디 클릭릭
+    img_스터디 = pa.locateCenterOnScreen('study.png')  # 이미지파일 센터값 저장
+    time.sleep(0.25)
+
+    pa.click(img_스터디) # 강의실 입장 센터값 클릭
+    pa.click(img_스터디) # 강의실 입장 센터값 클릭
+    time.sleep(1)
+
+    # 파일 우측클릭
+
+    loc_mp3 = pa.locateCenterOnScreen('mp3.png')  # 이미지파일 센터값 저장
+    time.sleep(0.25)
+
+    pa.moveTo(loc_mp3)
+    pa.mouseDown(button='right')
+
+    # 파일 저장하
+    loc_save = pa.locateCenterOnScreen('save.png')  # 이미지파일 센터값 저장
+    time.sleep(0.25)
+    pa.click(loc_save) # 강의실 입장 센터값 클릭
+    time.sleep(2)
+
+    loc_save_final = pa.locateCenterOnScreen('save_final.png')  # 이미지파일 센터값 저장
+    time.sleep(0.25)
+    pa.moveTo(loc_save_final) # 강의실 입장 센터값 클릭
+    pa.click(loc_save_final) # 강의실 입장 센터값 클릭
+    time.sleep(1)
+
+    #파일창 닫기
+    # loc_save = pa.locateCenterOnScreen('close.png')  # 이미지파일 센터값 저장
+    # time.sleep(0.25)
+    # pa.moveTo(loc_save) # 강의실 입장 센터값 클릭
+    # pa.click(loc_save) # 강의실 입장 센터값 클릭
+    pa.hotkey('command', 'w')
+    time.sleep(1)
+
+# 6부터 2씩 증가하여 126까
+# //*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[6]/td[3]
+# //*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[8]/td[3]
+# //*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[62]/td[3]
+# //*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[64]/td[3]
+# //*[@id="Table_02"]/tbody/tr[1]/td[2]/table[2]/tbody/tr[126]/td[3]
+
+
+
+pa.position()
+
+
+
+
+
+
+
+# ------------------- 테스트 ----------------------------
 
 pa.position()   #마우스 좌표 알아내기
 enter = pa.screenshot(region=(572, 430, 30, 30) ) # 강의실 입장하기 좌표
