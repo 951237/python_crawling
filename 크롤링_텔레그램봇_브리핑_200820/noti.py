fc65ae4e233eb67d47c342c49c70c9be8af23a50
951237 <951237@gmail.com>
2020-08-21 02:40:33 +0900

    Doc : Update

--- 크롤링_텔레그램봇_브리핑_200820/noti.py
+++ 크롤링_텔레그램봇_브리핑_200820/noti.py
@@ -1,9 +1,7 @@
-# from config import TELEGRAM_TOKEN, CHAT_ID
+from config import TELEGRAM_TOKEN, CHAT_ID
 import requests
 import telegram
 
-TELEGRAM_TOKEN = '토큰'
-CHAT_ID = '아이디'
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 def send(t):
