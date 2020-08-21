b7a05ce3dc5041f22733590bcbdc5ddc136d3ca3
951237 <951237@gmail.com>
2020-08-21 21:12:08 +0900

    Doc : Update - 토큰, 챗아이디 파일 추적 중지

--- 크롤링_텔레그램봇_브리핑_200820/noti.py
+++
@@ -1,9 +0,0 @@
-from config import TELEGRAM_TOKEN, CHAT_ID
-import requests
-import telegram
-
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-
-def send(t):
-    bot.sendMessage(CHAT_ID, t, parse_mode=telegram.ParseMode.HTML)
-
