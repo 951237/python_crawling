cec081aaf0fbdfaf5793b6190201da6e81cd547b
951237 <951237.gmail.com>
2021-01-18 22:39:26 +0900

    Doc : Create -  sqlite3 데이터 테이블 생성

---
+++ 크롤링_안산교육지원청_210117/sqlite3_create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE ansan_edu_office (
+    id text primary key,
+    department text not null,
+    position text,
+    _name text,
+    _charge integer not null,
+    create_time text not null
+);
+
