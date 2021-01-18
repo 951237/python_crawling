47e62f43c9dde3aa70a0166f9ea52c0e1e0150db
951237 <951237.gmail.com>
2021-01-18 14:42:42 +0900

    Doc : Create - 주석 추가

--- 크롤링_안산교육지원청_210117/210117_안산교육지원청 크롤링.py
+++ 크롤링_안산교육지원청_210117/210117_안산교육지원청 크롤링.py
@@ -6,10 +6,11 @@ import sqlite3
 from datetime import date
 from tqdm import tqdm
 
+# 크롤링 중 인증서 오류 해결 코드 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-PATH_DB = "/Users/mac/Documents/문서 - 951237's 2017 Macbook Pro/coding_python/python_db/db_crawling.db"
-DATE_TODAY = date.today().strftime('%y.%m.%d')
+PATH_DB = "/Users/mac/Documents/문서 - 951237's 2017 Macbook Pro/coding_python/python_db/db_crawling.db"    # 데이터베이스 경로
+DATE_TODAY = date.today().strftime('%y.%m.%d')  # 오늘 날짜 
 
 # 부서별 크롤링 위한 딕셔너리
 dic_url = {
@@ -40,14 +41,12 @@ def save_db(p_df):
     con = sqlite3.connect(PATH_DB)  #db에 접속
     p_df.to_sql('db_ansan_edu_office', con, index=False, index_label=False, if_exists='append')
 
-print('Starting mission')
-for k, v in tqdm(dic_url.items(), desc = 'DB생성'):
-    df = get_ansan_office_info(k, v)
-    save_db(df)
-print('Mission Complete!')
+if __name__ == "__main__":
+    print('Starting mission')
+    for k, v in tqdm(dic_url.items(), desc = 'DB생성'):
+        df = get_ansan_office_info(k, v)
+        save_db(df)
+    print('Mission Complete!')
 
 
-# sql DB생성하기
-# 데이터 프레임 DB저장하기 
-# 기존 DB와 비교해서 내용이 바뀐 경우 업데이트 하기 / 비고란에 업데이트 날짜 기록하기
 
