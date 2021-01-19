8fefec097d154b50d5c131d18d92854a2ed55de6
951237 <951237.gmail.com>
2021-01-20 04:29:33 +0900

    no message

--- 크롤링_안산교육지원청_210117/sqlite3_create_table.sql
+++ 크롤링_안산교육지원청_210117/sqlite3_create_table.sql
@@ -7,3 +7,11 @@ CREATE TABLE ansan_edu_office (
     create_time text not null
 );
 
+CREATE TABLE hakhyun_student_staus (
+    grade text primary key,
+    num_class integer,
+    num_boy integer,
+    num_girl integer,
+    total integer,
+    create_date text
+);
